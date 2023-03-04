515bd48ad28a246b197b467c556423d2e45aa357
parth-nikam <parthsnikam12co@gmail.com>
2023-03-04 18:09:24 +0530

    Register Button Created(Test1)

---
+++ VishayKa/Controller/RegisterViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RegisterViewController.swift
+//  VishayKa
+//
+//  Created by Parth Nikam on 04/03/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+
+    @IBAction func RegisterButton(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
