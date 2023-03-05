aa12a5ac3ddc33c369fdd18056c8a5ac193bbc83
parth-nikam <parthsnikam12co@gmail.com>
2023-03-05 09:50:04 +0530

    Welcome Screen Animation

--- VishayKa/Controllers/WelcomeViewController.swift
+++ VishayKa/Controllers/WelcomeViewController.swift
@@ -14,15 +14,6 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "💬 VishayKa"
-        for letter in titleText{
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false){ (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
 
        
     }
