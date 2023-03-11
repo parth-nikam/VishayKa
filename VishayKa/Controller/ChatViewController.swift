db890b9d43262f84d9061567daa640cf9819fef7
parth-nikam <parthsnikam12co@gmail.com>
2023-03-11 23:20:27 +0530

    Sort and Querey the Data

--- VishayKa/Controller/ChatViewController.swift
+++ VishayKa/Controller/ChatViewController.swift
@@ -34,7 +34,11 @@ class ChatViewController: UIViewController {
     
     func loadMessages(){
         messages = []
-        db.collection(K.FStore.collectionName).getDocuments{(QuerySnapshot, error) in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener{(QuerySnapshot, error) in
+            self.messages = []
+            
             if let e = error{
                 print("There was an error while reading data from Firestore. \(e)")
             }
@@ -62,7 +66,8 @@ class ChatViewController: UIViewController {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
             ]) { (error) in
                 if let e = error{
                     print("There was an issue saving data to Firestore. \(e)")
